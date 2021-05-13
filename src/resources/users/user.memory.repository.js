97023c6dd8a45b56cbadf602c9f733d550fa2dd1
BuianAlex <a.buyan@ukr.net>
2021-05-14 01:46:43 +0300

    feat: create user rout

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,42 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+const userList = [];
+
+const getAll = async () => userList;
+
+const pushUser = async (user) => {
+  userList.push(user);
+  return user;
 };
 
-module.exports = { getAll };
+const getById = async (userId) => {
+  const filteredArray = userList.filter((item) => item.id === userId);
+  if (filteredArray.length) {
+    return filteredArray[0];
+  }
+  return false;
+};
+
+const deleteById = async (userId) => {
+  const rowIndex = userList.findIndex((item) => item.id === userId);
+  if (rowIndex >= 0) {
+    userList.splice(rowIndex, 1);
+    return true;
+  }
+  return false;
+};
+
+const findByIdAndUpdate = async (userId, newProps) => {
+  console.log(newProps);
+  const rowIndex = userList.findIndex((item) => item.id === userId);
+  if (rowIndex >= 0) {
+    const user = userList[rowIndex];
+    Object.keys(newProps).forEach((key) => {
+      if (Object.prototype.hasOwnProperty.call(user, key)) {
+        user[key] = newProps[key];
+      }
+    });
+    return user;
+  }
+  return false;
+};
+
+module.exports = { getAll, pushUser, getById, deleteById, findByIdAndUpdate };
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,18 @@
 const usersRepo = require('./user.memory.repository');
+const User = require('./user.model');
 
 const getAll = () => usersRepo.getAll();
 
-module.exports = { getAll };
+const createUser = (userData) => {
+  const newUser = new User(userData);
+  return usersRepo.pushUser(newUser);
+};
+
+const getById = (userID) => usersRepo.getById(userID);
+
+const deleteById = (userID) => usersRepo.deleteById(userID);
+
+const findByIdAndUpdate = (userID, newProps) =>
+  usersRepo.findByIdAndUpdate(userID, newProps);
+
+module.exports = { getAll, createUser, getById, deleteById, findByIdAndUpdate };
