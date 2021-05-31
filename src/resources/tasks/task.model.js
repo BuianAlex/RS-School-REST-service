2e2e23718075a9db7dee6cba85595fd24ed7a88b
BuianAlex <a.buyan@ukr.net>
2021-05-31 11:54:03 +0300

    DOC: created doc comments for task object

--- src/resources/tasks/task.model.js
+++ src/resources/tasks/task.model.js
@@ -1,6 +1,18 @@
 const { v4: uuidv4 } = require('uuid');
-
+/**
+ * Class to create a task object
+ */
 class Task {
+  /**
+   * @param {Object} taskData  Data object for new task
+   * @param {string} taskData.id Task ID
+   * @param {string} taskData.boardId  Board ID with belong task
+   * @param {string} taskData.title Title of task
+   * @param {number} taskData.order Order for showing
+   * @param {string} taskData.description Task description
+   * @param {string} taskData.userId Owner User ID
+   * @param {string} taskData.columnId Column ID with belong task
+   */
   constructor({
     id = uuidv4(),
     title = 'testTask',
