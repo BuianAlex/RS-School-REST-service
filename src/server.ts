c306c83eec36e75234aa8f0bbf37d2da85bfa0ad
BuianAlex <a.buyan@ukr.net>
2021-06-12 09:42:54 +0300

    feat: add docker

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,20 @@
 import os from 'os';
 
 import options from './common/config';
+import sequelize from './dbConnect';
 import app from './app';
 
-app.listen(options.PORT, () =>
-  process.stdout.write(
-    `App is running on http://localhost:${options.PORT}${os.EOL}`
-  )
-);
+sequelize
+  .authenticate()
+  .then(() => {
+    process.stdout.write(`Connected to DB${os.EOL}`);
+    app.listen(options.PORT, () =>
+      process.stdout.write(
+        `App is running on http://localhost:${options.PORT}${os.EOL}`
+      )
+    );
+  })
+  .catch((err) => {
+    console.error(`Error: ${err}`);
+    process.exit(1);
+  });
